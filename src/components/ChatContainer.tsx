fa8da5635661ac6d794f23a5d4393b9a58f12857
om2137 <omraut961@gmail.com>
2026-01-12 21:52:12 +0530

    main chatbot container

---
+++ src/components/ChatContainer.tsx
@@ -0,0 +1,19 @@
+import ChatHeader from "./ChatHeader";
+import MessageList from "./MessageList";
+import ChatInput from "./ChatInput";
+import { useChatSession } from "../hooks/useChatSession";
+import { useChatApi } from "../hooks/useChatApi";
+
+export default function ChatContainer() {
+  const { sessionId, context } = useChatSession();
+  const { messages, sendMessage, typing } = useChatApi(sessionId, context);
+  console.log(sessionId)
+
+  return (
+    <div className="w-full h-full bg-white rounded-xl flex flex-col overflow-hidden">
+      <ChatHeader />
+      <MessageList messages={messages} typing={typing} onSend={sendMessage } />
+      <ChatInput onSend={sendMessage} disabled={!sessionId} />
+    </div>
+  );
+}
