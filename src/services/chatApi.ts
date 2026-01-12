5a9e002480b26c34fa22444df6cd846830eaceee
om2137 <omraut961@gmail.com>
2026-01-12 21:49:38 +0530

    service/utility function for api consumption

---
+++ src/services/chatApi.ts
@@ -0,0 +1,37 @@
+import type { UIInstruction } from "../types/chat";
+import { API_BASE_URL } from "./api";
+
+export async function initSession(
+  sessionId: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  context: any
+): Promise<void> {
+  await fetch(`${API_BASE_URL}/chat/session`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ sessionId, context })
+  });
+}
+
+export async function sendMessageApi(
+  sessionId: string,
+  message: string
+): Promise<{ reply: string; ui?: UIInstruction }> {
+  const res = await fetch(`${API_BASE_URL}/chat/message`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ sessionId, message })
+  });
+  
+  const data = await res.json();
+  console.log(data)
+  return data;
+}
+
+export async function fetchHistory(sessionId: string) {
+  const res = await fetch(
+    `${API_BASE_URL}/chat/history/${sessionId}`
+  );
+  const data = await res.json();
+  return data.messages;
+}
