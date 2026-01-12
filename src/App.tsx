99bacddea0f5689b2456caa3d51f0f4f0440b748
om2137 <omraut961@gmail.com>
2026-01-12 21:47:46 +0530

    root pages of frontend

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import ChatContainer from "./components/ChatContainer";
+
+export default function App() {
+  return (
+    <div className="h-screen">
+      <ChatContainer />
+    </div>
+  )
+
+}
